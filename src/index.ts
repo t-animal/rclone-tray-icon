127fa9d32743a57ae20300c749be38c5d597042d
Tilman Adler <Tilman.Adler@inovex.de>
2023-10-13 18:03:52 +0200

    Wrap code in main function

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,43 @@
-import { loadConfig } from "./config/config-parser";
-import { SyncTrayIcon } from "./tray/sync-tray-icon";
-import { YadTrayManager } from "./tray/tray-manager";
+import { loadConfig } from './config/config-parser';
+import { SyncTrayIcon } from './tray/sync-tray-icon';
+import { YadTrayManager } from './tray/tray-manager';
 
-try {
-  const config = loadConfig();
-  console.log(config);
-} catch (e) {
-  console.log((e as Error).message);
-}
-
-let isOn = true;
-
-const trayManager = new YadTrayManager();
-const syncTrayIcon = new SyncTrayIcon(trayManager);
-syncTrayIcon.initializeMenu();
-
-function reflectState() {
-  if (isOn) {
-    syncTrayIcon.setSyncEnabledAndDone();
-  } else {
-    syncTrayIcon.setSyncDisabled();
-  }
-}
-
-syncTrayIcon.addEventListener((event) => {
-  if (event === "quit") {
-    syncTrayIcon.close();
-    process.exit();
+function main() {
+  try {
+    const config = loadConfig();
+    console.log(config);
+  } catch (e) {
+    console.log((e as Error).message);
   }
 
-  if (event === "toggle-syncing") {
-    isOn = !isOn;
+  let isOn = true;
+
+  const trayManager = new YadTrayManager();
+  const syncTrayIcon = new SyncTrayIcon(trayManager);
+  syncTrayIcon.initializeMenu();
+
+  function reflectState() {
+    if (isOn) {
+      syncTrayIcon.setSyncEnabledAndDone();
+    } else {
+      syncTrayIcon.setSyncDisabled();
+    }
   }
+
+  syncTrayIcon.addEventListener((event) => {
+    if (event === "quit") {
+      syncTrayIcon.close();
+      process.exit();
+    }
+
+    if (event === "toggle-syncing") {
+      isOn = !isOn;
+    }
+
+    reflectState();
+  });
 
   reflectState();
-});
+}
 
-reflectState();
+main();
