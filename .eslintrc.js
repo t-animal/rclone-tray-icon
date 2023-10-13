4d6502d8da3d7c30492cf74775b764402fc8f5d8
Tilman Adler <Tilman.Adler@inovex.de>
2023-10-13 18:03:40 +0200

    Disable conflicting linter rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,10 +25,5 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
-  rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-  },
+  rules: {},
 };
